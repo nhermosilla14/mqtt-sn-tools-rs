b93debd444f9b30ac5a22e2f47841e6e751f9879
Nicolás Hermosilla P. <nhermosilla14@gmail.com>
2024-05-16 01:51:50 -0400

    Add SensorNetwork

--- src/mqttsn/network_abstractions.rs
+++ src/mqttsn/network_abstractions.rs
@@ -8,6 +8,9 @@
 // For now, only use UDP
 
 use std::net::UdpSocket;
+// SerialPort
+use serialport::SerialPort;
+use std::io::prelude::*;
 use crate::mqttsn::settings::Settings;
 
 // Create a connection to the MQTT-SN broker
@@ -25,4 +28,139 @@ pub fn mqtt_sn_create_connection(settings: &Settings) -> UdpSocket {
         .expect("Could not connect to MQTT-SN broker");
 
     socket
+}
+
+pub trait SensorNetwork {
+    fn new(source_address: &str, destination_address: &str, timeout: u64) -> Self;
+    fn initialize(&self);
+    fn get_description(&self) -> String;
+    fn receive(&mut self) -> Vec<u8>;
+    fn send(&mut self, data: &[u8]) -> bool;
+    fn get_timeout(&self) -> u64;
+    fn close(&self);
+}
+
+
+// SensorNetworks
+
+pub struct UDPSensorNetwork {
+    source_address: String,
+    destination_address: String,
+    socket: UdpSocket,
+    timeout: u64,
+}
+
+impl SensorNetwork for UDPSensorNetwork {
+    fn new(
+        source_address: &str,
+        destination_address: &str,
+        timeout: u64,
+    ) -> UDPSensorNetwork {
+        UDPSensorNetwork {
+            source_address: String::from(source_address),
+            destination_address: String::from(destination_address),
+            socket: UdpSocket::bind(source_address)
+                .expect("Could not bind to address"),
+            timeout,
+        }
+    }
+
+    fn get_timeout(&self) -> u64 {
+        self.timeout
+    }
+
+    fn initialize(&self) {
+        // Connect to the destination address
+        self.socket.connect(&self.destination_address)
+            .expect("Could not connect to destination address");
+
+        // Set the timeout
+        self.socket.set_read_timeout(Some(std::time::Duration::from_secs(self.timeout)))
+            .expect("Could not set read timeout");
+
+        // Set the socket to non-blocking
+        self.socket.set_nonblocking(true)
+            .expect("Could not set socket to non-blocking");
+    }
+
+    fn get_description(&self) -> String {
+        format!("UDP Sensor Network: Source: {}, Destination: {}", self.source_address, self.destination_address)
+    }
+
+   fn send(&mut self, data: &[u8]) -> bool {
+        self.socket.send(data)
+            .expect("Could not send data");
+        true
+    }
+
+    fn receive(&mut self) -> Vec<u8> {
+        let mut buffer = [0; 1024];
+        let (size, _) = self.socket.recv_from(&mut buffer)
+            .expect("Could not receive data");
+        buffer[0..size].to_vec()
+    }
+
+    fn close(&self) {
+        // Nothing to do here
+    }
+}
+
+
+// SerialPortSensorNetwork
+
+pub struct SerialPortSensorNetwork {
+    source_address: String, // Baudrate
+    destination_address: String, // Serial port name
+    timeout: u64,
+    port: Box<dyn SerialPort>,
+}
+
+impl SensorNetwork for SerialPortSensorNetwork {
+    fn new(
+        source_address: &str,
+        destination_address: &str,
+        timeout: u64,
+    ) -> SerialPortSensorNetwork {
+        let baudrate = source_address.parse::<u32>().expect("Could not parse baudrate");
+        let port = serialport::new(destination_address, baudrate)
+            .timeout(std::time::Duration::from_secs(timeout))
+            .open()
+            .expect("Could not open serial port");
+
+        SerialPortSensorNetwork {
+            source_address: String::from(source_address),
+            destination_address: String::from(destination_address),
+            timeout,
+            port,
+        }
+    }
+
+    fn get_timeout(&self) -> u64 {
+        self.timeout
+    }
+
+    fn initialize(&self) {
+        // Nothing to do here
+    }
+
+    fn get_description(&self) -> String {
+        format!("Serial Port Sensor Network: Source: {}, Destination: {}", self.source_address, self.destination_address)
+    }
+
+    fn send(&mut self, data: &[u8]) -> bool {
+        self.port.write_all(data)
+            .expect("Could not write data to serial port");
+        true    
+    }
+
+    fn receive(&mut self) -> Vec<u8> {
+        let mut buffer = [0; 1024];
+        let size = self.port.read(&mut buffer)
+            .expect("Could not read data from serial port");
+        buffer[0..size].to_vec()
+    }
+
+    fn close(&self) {
+       // Nothing to do here 
+    }
 }
